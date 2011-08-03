5b2e369ab30f605251879d6f1ba21725af503e39
X1337X <pokemonmasterjack@hotmail.co.uk>
2011-08-03 14:32:16 +0100

    added plugin.yml-plugin can now be ran with bukkit

--- src/x1337x/matter123/quicktown/townmaker/TownMaker.java
+++ src/x1337x/matter123/quicktown/townmaker/TownMaker.java
@@ -1,8 +1,13 @@
 package x1337x.matter123.quicktown.townmaker;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import x1337x.matter123.quicktown.townmaker.commands.CreateCommand;
 import x1337x.matter123.quicktown.townmaker.exceptions.PluginMissingException;
 
 import com.sk89q.worldedit.WorldEdit;
@@ -13,25 +18,37 @@ public class TownMaker extends JavaPlugin {
 public static WorldEdit worldedit = null;
 public static WorldEditAPI worldeditapi = null;
 public static WorldEditPlugin worldeditplugin = null;
+public TownMaker instance = null;
+Logger log = this.getServer().getLogger();
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
-		
+		TownMaker.worldedit = null;
+	    TownMaker.worldeditapi = null;
+	    TownMaker.worldeditplugin = null;
+	    this.instance = null;
+	    log(Level.INFO,"Made by matters123 and 1337 is enabled");
+	    
 	}
+
 
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
 	try {
+		this.instance = this;
 		worldeditplugin = getWorldEdit();
 		worldeditapi = worldeditplugin.getAPI();
         worldedit = worldeditplugin.getWorldEdit();
+        this.getCommand("town").setExecutor(new CreateCommand(this));
 		
 	} catch (PluginMissingException e) {
 		// TODO Auto-generated catch block
+		log(Level.SEVERE, e.getMessage());
 		this.getServer().getPluginManager().disablePlugin(this);
 	}
+	log(Level.INFO,"Made by matters123 and 1337 is enabled");
 	}
 public WorldEditPlugin getWorldEdit() throws PluginMissingException{
 	Plugin worldedit = this.getServer().getPluginManager().getPlugin("World Edit");
@@ -39,8 +56,22 @@ public WorldEditPlugin getWorldEdit() throws PluginMissingException{
 	if(worldedit == null){
 		throw new PluginMissingException(new Throwable(), " could not be found disabling..", "World Edit");
 	}
+	
 	else{
 		return (WorldEditPlugin) worldedit;
 	}
+}
+public String getName()
+{
+  PluginDescriptionFile pdfFile = getDescription();
+  return pdfFile.getName();
+}
+public String getVersion() {
+  PluginDescriptionFile pdfFile = getDescription();
+  return pdfFile.getVersion();
+}
+public void log(Level level,String message){
+	String newmessage = "[" + getName() + " : " + getVersion() + "] " + message;
+	log.log(level, newmessage);
 }
 }
